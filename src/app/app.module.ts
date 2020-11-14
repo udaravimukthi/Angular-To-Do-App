9412b2723d4acdc75a7f4614902e7dd3496514ef
udara <uvlakshan@gmail.com>
2020-11-14 17:57:45 +0530

    added new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { TodoNewItemComponent } from './todo-new-item/todo-new-item.component';
 import { CompletedItemComponent } from './completed-item/completed-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -17,8 +23,14 @@ import { CompletedItemComponent } from './completed-item/completed-item.componen
     CompletedItemComponent
   ],
   imports: [
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
